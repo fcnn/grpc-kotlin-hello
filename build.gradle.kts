993275056cd4f2498172c344df51a15fd305e2bd
root <root@t>
2020-07-28 16:57:46 +0800

    up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 
-val gradle_version = "6.6-rc-2"
+val gradle_version = "6.6-rc-3"
 
 buildscript {
     extra.apply {
         set("grpcVersion", "+")
-        set("kotlinVersion", "1.4-M3")
+        set("kotlinVersion", "1.4.0-rc")
     }
 
     repositories {
